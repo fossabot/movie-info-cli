2cabcd5e384f9883486adcdccc7ae4ea54364ada
phongduong <s2pkmol@gmail.com>
2018-05-10 22:30:15 +0700

    update API

--- movie-info.js
+++ movie-info.js
@@ -1,12 +1,35 @@
 #!/usr/bin/env node
 
 require("dotenv").config();
+
 const axios = require("axios");
 const program = require("commander");
 const inquirer = require("inquirer");
 
 program
-  .version("0.1.0")
-  .option("-a --about", "About command")
-  .option("-")
-  .parse(process.argv);
+  .version("1.0.0", "-v --version")
+  .usage("[options] <id> [more]")
+  .option("-l --list", "Get all movie list")
+  .option("-i, --id <id>", "Search movie by id")
+  .option("-m --more", "More options");
+
+program.parse(process.argv);
+
+if (program.id) {  
+  if (!!program.more) {
+
+  } else {
+    axios
+      .get(process.env.API_GET_MOVIE_DETAIL, {
+        params: {
+          movie_id: program.id,
+          with_images: true,
+          with_cast: true
+        }
+      })
+      .then(response => {
+        JSON.stringify(response.data["data"]["movie"], null, " ");
+      })
+      .catch(error => console.log(error));
+  }
+}
